c5c1da987d8081011d956f37a1ce2790c2ec7bb4
Joseph IDO <joseph.ido@example.com>
2025-08-08 00:02:36 +0200

    adding overlay module second commit

--- components/overlay/Overlay.tsx
+++ components/overlay/Overlay.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
-import styles from './Overlay.module.css'
+import styles from './overlay.module.css'
+import { Landingpagemodule } from '../landingpagemodules/Landingpagemodule'
+import closeIcon from '../../src/assets/close.png'
 
+type overlayprops ={
+    children: React.ReactNode;
 
-export const Overlay = () => {
+}
+
+export const Overlay: React.FC<React.ReactNode>= () => {
   return (
-    <div>Overlay</div>
+    <div>
+        <image className = {styles['close-icon']} src={closeIcon} onClick={} /> 
+        <text className={styles.overlaytext}> this is the overlay component</text> 
+
+    </div>
   )
 }
